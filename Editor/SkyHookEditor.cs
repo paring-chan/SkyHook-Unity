317967423f3aaeb6c075ed21f21d60d3f4165eef
ChocoSwi <channel.iru0000@gmail.com>
2022-11-05 23:29:49 +0900

    feat: require focus toggle

--- Editor/SkyHookEditor.cs
+++ Editor/SkyHookEditor.cs
@@ -11,9 +11,12 @@ namespace SkyHook.Editor
         public override void OnInspectorGUI()
         {
             SkyHookManager manager = (SkyHookManager)target;
+            
             GUI.enabled = false;
             EditorGUILayout.Toggle("Hook Started", manager.isHookActive);
             GUI.enabled = true;
+            
+            EditorGUILayout.Toggle("Require Focus", manager.requireFocus);
         }
     }
 }
