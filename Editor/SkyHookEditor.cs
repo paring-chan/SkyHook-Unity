f13e1e985e69114179128f78dddafccf53274354
ChocoSwi <channel.iru0000@gmail.com>
2022-11-05 23:25:09 +0900

    feat: show hook started

--- Editor/SkyHookEditor.cs
+++ Editor/SkyHookEditor.cs
@@ -11,6 +11,9 @@ namespace SkyHook.Editor
         public override void OnInspectorGUI()
         {
             SkyHookManager manager = (SkyHookManager)target;
+            GUI.enabled = false;
+            EditorGUILayout.Toggle("Hook Started", manager.isHookActive);
+            GUI.enabled = true;
         }
     }
 }
